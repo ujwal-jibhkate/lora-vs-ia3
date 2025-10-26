9e80458cfb69e93c99a02e09b89444f304eb4fae
Ujwal Jibhkate <ujwaljibhkate@gmail.com>
2025-10-25 22:07:53 -0400

    metrics.py update

--- src/metrics.py
+++ src/metrics.py
@@ -37,24 +37,24 @@ def compute_metrics(eval_pred: EvalPrediction, task_name: str, tokenizer_name: s
 
     # We are temporarily disabling samsum due to local install issues.
     #
-    # elif task_name == "samsum":
-    #     rouge_metric = evaluate.load("rouge") # <-- Load on the fly
-    #     tokenizer = _get_tokenizer(tokenizer_name, token=token)
-    #
-    #     predictions[predictions == -100] = tokenizer.pad_token_id
-    #     decoded_preds = tokenizer.batch_decode(predictions, skip_special_tokens=True)
-    #
-    #     labels[labels == -100] = tokenizer.pad_token_id
-    #     decoded_labels = tokenizer.batch_decode(labels, skip_special_tokens=True)
-    #
-    #     decoded_preds = ["\n".join(pred.split()) for pred in decoded_preds]
-    #     decoded_labels = ["\n".join(label.split()) for label in decoded_labels]
-    #
-    #     result = rouge_metric.compute(
-    #         predictions=decoded_preds, references=decoded_labels, use_stemmer=True
-    #     )
-    #     result = {key: value * 100 for key, value in result.items()}
-    #     return {k: round(v, 4) for k, v in result.items()}
+    elif task_name == "samsum":
+        rouge_metric = evaluate.load("rouge") # <-- Load on the fly
+        tokenizer = _get_tokenizer(tokenizer_name, token=token)
+    
+        predictions[predictions == -100] = tokenizer.pad_token_id
+        decoded_preds = tokenizer.batch_decode(predictions, skip_special_tokens=True)
+    
+        labels[labels == -100] = tokenizer.pad_token_id
+        decoded_labels = tokenizer.batch_decode(labels, skip_special_tokens=True)
+    
+        decoded_preds = ["\n".join(pred.split()) for pred in decoded_preds]
+        decoded_labels = ["\n".join(label.split()) for label in decoded_labels]
+    
+        result = rouge_metric.compute(
+            predictions=decoded_preds, references=decoded_labels, use_stemmer=True
+        )
+        result = {key: value * 100 for key, value in result.items()}
+        return {k: round(v, 4) for k, v in result.items()}
     
     elif task_name == "dolly":
         return {} # Let the Trainer handle loss calculation
